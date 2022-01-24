2baad01c48f9519435f2fff8ec748d5f208430f2
ro <kronew6@gmail.com>
2022-01-24 17:40:47 -0600

    edits create action in dogs controller to render login error

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,15 +1,23 @@
 class DogsController < ApplicationController
+  
   def create
-    dog = Dog.new(
-      name: params[:name],
-      age: params[:age],
-      breed: params[:breed],
-      user_id: current_user.id
-    )
-    if dog.save
-      render json: dog.as_json
+    # if user is logged in they can go on.....
+    if current_user
+      dog = Dog.new(
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed],
+        user_id: current_user.id
+      )
+      if dog.save
+        render json: dog.as_json
+      else
+        render json: {errors: dog.errors.full_messages}, status: :unprocessable_entity
+      end
     else
-      render json: {errors: dog.errors.full_messages}, status: :unprocessable_entity
+      # 3 render statements ok??????????
+      render json: {Error: "Please log in first."}
     end
   end
+
 end
