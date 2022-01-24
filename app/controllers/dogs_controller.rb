0cefa7ca1610f9ff41e96875caaf331e047d3511
ro <kronew6@gmail.com>
2022-01-24 15:49:00 -0600

    adds create action to dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+  def create
+    dog = Dog.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+      user_id: current_user
+    )
+    if dog.save
+      render json: dog.as_json
+    else
+      render json: {errors: dog.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 end
